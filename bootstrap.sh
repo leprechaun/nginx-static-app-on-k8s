349d5107fe2f78d59bfb9023f020ff4195d0acac
Laurence MacGuire <leprechaun@gmail.com>
2017-08-12 20:00:30 +0700

    OS_BUILD_SOURCE had a : ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,10 @@ set -x
 
 cd /usr/share/nginx/html/
 
-sed -i \
-  -e "s:\${HOSTNAME}:${HOSTNAME}:g" \
-  -e "s:\${OPENSHIFT_BUILD_NAME}:${OPENSHIFT_BUILD_NAME}:g" \
-  -e "s:\${OPENSHIFT_BUILD_NAMESPACE}:${OPENSHIFT_BUILD_NAMESPACE}:g" \
-  -e "s:\${OPENSHIFT_BUILD_SOURCE}:${OPENSHIFT_BUILD_SOURCE}:g" \
-  -e "s:\${OPENSHIFT_BUILD_COMMIT}:${OPENSHIFT_BUILD_COMMIT}:g" index.html
+sed -i -e "s:\${HOSTNAME}:${HOSTNAME}:g" index.html
+sed -i -e "s:\${OPENSHIFT_BUILD_NAME}:${OPENSHIFT_BUILD_NAME}:g" index.html
+sed -i -e "s:\${OPENSHIFT_BUILD_NAMESPACE}:${OPENSHIFT_BUILD_NAMESPACE}:g" index.html
+sed -i -e "s#\${OPENSHIFT_BUILD_SOURCE}#${OPENSHIFT_BUILD_SOURCE}#g" index.html
+sed -i -e "s:\${OPENSHIFT_BUILD_COMMIT}:${OPENSHIFT_BUILD_COMMIT}:g" index.html
 
 exec "${@}"
