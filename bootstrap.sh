00dfbfdd0fc992c505d693c9ddd6e64c740e8132
Laurence MacGuire <leprechaun@gmail.com>
2017-08-12 19:39:18 +0700

    Also need the bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+cd /usr/share/nginx/html/
+
+sed -i \
+  -e "s:\${HOSTNAME}:${HOSTNAME}:g" \
+  -e "s:\${OPENSHIFT_BUILD_NAME}:${OPENSHIFT_BUILD_NAME}:g" \
+  -e "s:\${OPENSHIFT_BUILD_NAMESPACE}:${OPENSHIFT_BUILD_NAMESPACE}:g" \
+  -e "s:\${OPENSHIFT_BUILD_SOURCE}:${OPENSHIFT_BUILD_SOURCE}:g" \
+  -e "s:\${OPENSHIFT_BUILD_COMMIT}:${OPENSHIFT_BUILD_COMMIT}:g" index.html
+
+exec "${@}"
